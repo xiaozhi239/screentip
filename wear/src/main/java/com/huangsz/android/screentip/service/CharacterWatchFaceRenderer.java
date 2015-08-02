b33e8bfec6b6611e3eb769f4b572875255da3ad1
Shuzhi Huang <huangsz@google.com>
2015-08-02 20:46:56 +0100

    Add a CharacterWatchFaceRenderer class to be in charge of drawing watch face.

---
+++ wear/src/main/java/com/huangsz/android/screentip/service/CharacterWatchFaceRenderer.java
@@ -0,0 +1,211 @@
+package com.huangsz.android.screentip.service;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+
+import com.huangsz.android.screentip.R;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+/**
+ * Responsible for the drawing of character watch face.
+ */
+public class CharacterWatchFaceRenderer {
+
+    // Text paint typefaces.
+    private static final Typeface BOLD_TYPEFACE =
+            Typeface.create(Typeface.SERIF, Typeface.BOLD);
+    private static final Typeface NORMAL_TYPEFACE =
+            Typeface.create(Typeface.SERIF, Typeface.NORMAL);
+
+    // Constant to help calculate clock hand rotations
+    private static final float TWO_PI = (float) Math.PI * 2f;
+
+    private Context mContext;
+
+    private Calendar mCalendar;
+
+    private boolean mIsInAmbientMode;
+
+    // properties
+    private boolean mLowBitAmbient = false;
+    private boolean mBurnInProtection = false;
+
+    // graphic objects
+    private Bitmap mBackgroundBitmap;
+    private Bitmap mBackgroundScaledBitmap;
+    private Paint mMinTickPaint;
+    private Paint mSecTickPaint;
+    private Paint mHourPaint;
+    private Paint mMinutePaint;
+    private Paint mSecondPaint;
+    private Paint mCharacterPaint;  // Paint to show a character as a reminder tip.
+
+    // TODO(huangsz) Receive this from handheld device.
+    private static final String TIP_TEXT = "坚";
+
+    public CharacterWatchFaceRenderer(Context context) {
+        this.mContext = context;
+
+        // load the background image
+        Resources resources = mContext.getResources();
+        Drawable backgroundDrawable = resources.getDrawable(R.drawable.background, null);
+        mBackgroundBitmap = ((BitmapDrawable) backgroundDrawable).getBitmap();
+
+        // create graphic styles
+        mMinTickPaint = createLinePaint(resources.getColor(R.color.minute_tick_color),
+                2.f, Paint.Cap.ROUND);
+        mSecTickPaint = createLinePaint(resources.getColor(R.color.second_tick_color),
+                1.f, Paint.Cap.BUTT);
+        mHourPaint = createLinePaint(
+                resources.getColor(R.color.hour_bar_color), 5.f, Paint.Cap.ROUND);
+        mMinutePaint = createLinePaint(
+                resources.getColor(R.color.minute_bar_color), 4.f, Paint.Cap.ROUND);
+        mSecondPaint = createLinePaint(
+                resources.getColor(R.color.second_bar_color), 2.f, Paint.Cap.BUTT);
+
+        mCharacterPaint = createTextPaint(resources, false);
+
+        // allocate a Calendar to calculate local time using the UTC time and time zone
+        mCalendar = Calendar.getInstance();
+    }
+
+    public void setTimeZone(TimeZone timeZone) {
+        mCalendar.setTimeZone(timeZone);
+    }
+
+    public void setCharacterColor(int color) {
+        mCharacterPaint.setColor(color);
+    }
+
+    public void setTickColor(int color) {
+        mMinTickPaint.setColor(color);
+        mSecTickPaint.setColor(color);
+    }
+
+    public void setProperties(boolean lowBitAmbient, boolean burnInProtection) {
+        mLowBitAmbient = lowBitAmbient;
+        mBurnInProtection = burnInProtection;
+    }
+
+    public void setAmbientMode(boolean isInAmbientMode) {
+        mIsInAmbientMode = isInAmbientMode;
+        if (mLowBitAmbient) {
+            boolean antiAlias = !isInAmbientMode;
+            mHourPaint.setAntiAlias(antiAlias);
+            mMinutePaint.setAntiAlias(antiAlias);
+            // mSecondPaint is not presented in ambient mode anyway.
+            mMinTickPaint.setAntiAlias(antiAlias);
+            mCharacterPaint.setAntiAlias(antiAlias);
+        }
+        // TODO(huangsz) restore to black and white.
+    }
+
+    public void onSurfaceChanged(int width, int height) {
+        if (mBackgroundScaledBitmap == null
+                || mBackgroundScaledBitmap.getWidth() != width
+                || mBackgroundScaledBitmap.getHeight() != height) {
+            mBackgroundScaledBitmap = Bitmap.createScaledBitmap(mBackgroundBitmap,
+                    width, height, true /* filter */);
+        }
+    }
+
+    public void draw(Canvas canvas, Rect bounds) {
+        // Update the time
+        mCalendar.setTimeInMillis(System.currentTimeMillis());
+
+        int width = bounds.width();
+        int height = bounds.height();
+
+        canvas.drawBitmap(mBackgroundScaledBitmap, 0, 0, null);
+
+        // Find the center. Ignore the window insets so that, on round watches
+        // with a "chin", the watch face is centered on the entire screen, not
+        // just the usable portion.
+        float centerX = width / 2f;
+        float centerY = height / 2f;
+
+        // Draw the ticks.
+        drawTicks(canvas, centerX, centerY, 10, 12, mMinTickPaint);
+        drawTicks(canvas, centerX, centerY, 3, 60, mSecTickPaint);
+
+        // Compute rotations and lengths for the clock hands.
+        float seconds = mCalendar.get(Calendar.SECOND) +
+                mCalendar.get(Calendar.MILLISECOND) / 1000f;
+        float secRot = seconds / 60f * TWO_PI;
+        float minutes = mCalendar.get(Calendar.MINUTE) + seconds / 60f;
+        float minRot = minutes / 60f * TWO_PI;
+        float hours = mCalendar.get(Calendar.HOUR) + minutes / 60f;
+        float hrRot = hours / 12f * TWO_PI;
+
+        float secLength = centerX - 20;
+        float minLength = centerX - 40;
+        float hrLength = centerX - 60;
+
+        // Only draw the second hand in interactive mode.
+        if (!mIsInAmbientMode) {
+            float secX = (float) Math.sin(secRot) * secLength;
+            float secY = (float) -Math.cos(secRot) * secLength;
+            canvas.drawLine(centerX, centerY, centerX + secX, centerY +
+                    secY, mSecondPaint);
+        }
+
+        // Draw the minute and hour hands.
+        float minX = (float) Math.sin(minRot) * minLength;
+        float minY = (float) -Math.cos(minRot) * minLength;
+        canvas.drawLine(centerX, centerY, centerX + minX, centerY + minY,
+                mMinutePaint);
+        float hrX = (float) Math.sin(hrRot) * hrLength;
+        float hrY = (float) -Math.cos(hrRot) * hrLength;
+        canvas.drawLine(centerX, centerY, centerX + hrX, centerY + hrY,
+                mHourPaint);
+
+        // Draw the character.
+        float charX = centerX;
+        float charY = centerY * 1.6f;
+        canvas.drawText(TIP_TEXT, charX, charY, mCharacterPaint);
+    }
+
+    private void drawTicks(Canvas canvas, float centerX, float centerY,
+                           int length, int divisions, Paint paint) {
+        float outerTickRadius = centerX;
+        float innerTickRadius = centerX - length;
+        for (int tickIndex = 0; tickIndex < divisions; tickIndex++) {
+            float tickRot = tickIndex * TWO_PI / divisions;
+            float innerX = (float) Math.sin(tickRot) * innerTickRadius;
+            float innerY = (float) -Math.cos(tickRot) * innerTickRadius;
+            float outerX = (float) Math.sin(tickRot) * outerTickRadius;
+            float outerY = (float) -Math.cos(tickRot) * outerTickRadius;
+            canvas.drawLine(centerX + innerX, centerY + innerY,
+                    centerX + outerX, centerY + outerY, paint);
+        }
+    }
+
+    private Paint createLinePaint(int color, float strokeWidth, Paint.Cap strokeCap) {
+        Paint paint = new Paint();
+        paint.setColor(color);
+        paint.setStrokeWidth(strokeWidth);
+        paint.setStrokeCap(strokeCap);
+        paint.setAntiAlias(true);
+        return paint;
+    }
+
+    private Paint createTextPaint(Resources resources, boolean isBold) {
+        Paint paint = new Paint();
+        paint.setColor(resources.getColor(R.color.black));
+        paint.setAntiAlias(true);
+        paint.setTextAlign(Paint.Align.CENTER);
+        paint.setTypeface(isBold ? BOLD_TYPEFACE : NORMAL_TYPEFACE);
+        paint.setTextSize(resources.getDimension(R.dimen.tip_text_size));
+        paint.setColor(resources.getColor(R.color.tip_text_color));
+        return paint;
+    }
+}
--- wear/src/main/java/com/huangsz/android/screentip/service/CharacterWatchFaceService.java
+++ wear/src/main/java/com/huangsz/android/screentip/service/CharacterWatchFaceService.java
@@ -4,15 +4,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.Rect;
-import android.graphics.Typeface;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -32,19 +26,12 @@ import com.google.android.gms.wearable.DataItemBuffer;
 import com.google.android.gms.wearable.DataMap;
 import com.google.android.gms.wearable.DataMapItem;
 import com.google.android.gms.wearable.Wearable;
-import com.huangsz.android.screentip.R;
 
-import java.util.Calendar;
 import java.util.TimeZone;
 
 public class CharacterWatchFaceService extends CanvasWatchFaceService {
 
     private static final String TAG = "CharWatchFaceService";
-
-    private static final Typeface BOLD_TYPEFACE =
-            Typeface.create(Typeface.SERIF, Typeface.BOLD);
-    private static final Typeface NORMAL_TYPEFACE =
-            Typeface.create(Typeface.SERIF, Typeface.NORMAL);
 
     @Override
     public Engine onCreateEngine() {
@@ -55,9 +42,6 @@ public class CharacterWatchFaceService extends CanvasWatchFaceService {
     /* implement service callback methods */
     private class Engine extends CanvasWatchFaceService.Engine implements
             GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
-
-        // Constant to help calculate clock hand rotations
-        private static final float TWO_PI = (float) Math.PI * 2f;
 
         // Keep the same with {@link WatchFaceConfigActivity} in handheld app.
         private static final String DATA_LAYER_WATCHFACE_CONFIG_PATH = "/watch_face_config";
@@ -72,27 +56,11 @@ public class CharacterWatchFaceService extends CanvasWatchFaceService {
 
         private static final int INTERACTIVE_UPDATE_RATE_MS = 1000;
 
+        private CharacterWatchFaceRenderer mWatchFaceRenderer;
+
         private boolean mTimeZoneReceiverRegistered = false;
 
-        private boolean mLowBitAmbient = false;
-        private boolean mBurnInProtection = false;
-
-        private Calendar mCalendar;
-
-        // graphic objects
-        private Bitmap mBackgroundBitmap;
-        private Bitmap mBackgroundScaledBitmap;
-        private Paint mMinTickPaint;
-        private Paint mSecTickPaint;
-        private Paint mHourPaint;
-        private Paint mMinutePaint;
-        private Paint mSecondPaint;
-        private Paint mCharacterPaint;  // Paint to show a character as a reminder tip.
-
         private GoogleApiClient mGoogleApiClient;
-
-        // TODO(huangsz) Receive this from handheld device.
-        private static final String TIP_TEXT = "坚";
 
         /**
          * handler to update the time once a second in interactive mode
@@ -121,7 +89,7 @@ public class CharacterWatchFaceService extends CanvasWatchFaceService {
         final BroadcastReceiver mTimeZoneReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                mCalendar.setTimeZone(TimeZone.getDefault());
+                mWatchFaceRenderer.setTimeZone(TimeZone.getDefault());
                 invalidate();
             }
         };
@@ -135,7 +103,6 @@ public class CharacterWatchFaceService extends CanvasWatchFaceService {
                         processConfigurationFor(item);
                     }
                 }
-
                 dataEvents.release();
                 invalidate();
             }
@@ -159,11 +126,11 @@ public class CharacterWatchFaceService extends CanvasWatchFaceService {
                 DataMap dataMap = DataMapItem.fromDataItem(item).getDataMap();
                 if (dataMap.containsKey(TAG_CHARACTER_COLOR)) {
                     String color = dataMap.getString(TAG_CHARACTER_COLOR);
-                    mCharacterPaint.setColor(Color.parseColor(color));
+                    mWatchFaceRenderer.setCharacterColor(Color.parseColor(color));
                 }
                 if (dataMap.containsKey(TAG_TICK_COLOR)) {
                     String color = dataMap.getString(TAG_TICK_COLOR);
-                    mMinTickPaint.setColor(Color.parseColor(color));
+                    mWatchFaceRenderer.setTickColor(Color.parseColor(color));
                 }
             }
         }
@@ -184,27 +151,7 @@ public class CharacterWatchFaceService extends CanvasWatchFaceService {
                     .setShowSystemUiTime(false)
                     .build());
 
-            // load the background image
-            Resources resources = CharacterWatchFaceService.this.getResources();
-            Drawable backgroundDrawable = resources.getDrawable(R.drawable.background, null);
-            mBackgroundBitmap = ((BitmapDrawable) backgroundDrawable).getBitmap();
-
-            // create graphic styles
-            mMinTickPaint = createLinePaint(resources.getColor(R.color.minute_tick_color),
-                    2.f, Paint.Cap.ROUND);
-            mSecTickPaint = createLinePaint(resources.getColor(R.color.second_tick_color),
-                    1.f, Paint.Cap.BUTT);
-            mHourPaint = createLinePaint(
-                    resources.getColor(R.color.hour_bar_color), 5.f, Paint.Cap.ROUND);
-            mMinutePaint = createLinePaint(
-                    resources.getColor(R.color.minute_bar_color), 4.f, Paint.Cap.ROUND);
-            mSecondPaint = createLinePaint(
-                    resources.getColor(R.color.second_bar_color), 2.f, Paint.Cap.BUTT);
-
-            mCharacterPaint = createTextPaint(resources, false);
-
-            // allocate a Calendar to calculate local time using the UTC time and time zone
-            mCalendar = Calendar.getInstance();
+            mWatchFaceRenderer = new CharacterWatchFaceRenderer(CharacterWatchFaceService.this);
 
             mGoogleApiClient = new GoogleApiClient.Builder(CharacterWatchFaceService.this)
                     .addApi(Wearable.API)
@@ -222,90 +169,29 @@ public class CharacterWatchFaceService extends CanvasWatchFaceService {
             // For devices that require burn-in protection, avoid using large blocks of white
             // pixels in ambient mode and do not place content within 10 pixels of the edge of
             // the screen, since the system shifts the content periodically to avoid pixel burn-in.
-            mLowBitAmbient = properties.getBoolean(PROPERTY_LOW_BIT_AMBIENT, false);
-            mBurnInProtection = properties.getBoolean(PROPERTY_BURN_IN_PROTECTION,
-                    false);
+            mWatchFaceRenderer.setProperties(
+                    properties.getBoolean(PROPERTY_LOW_BIT_AMBIENT, false),
+                    properties.getBoolean(PROPERTY_BURN_IN_PROTECTION, false));
         }
 
         @Override
         public void onTimeTick() {
+            // In ambient mode, the system calls this method every one minute.
             super.onTimeTick();
-            // In ambient mode, most watch face implementations simply invalidate the canvas to
-            // redraw the watch face in the Engine.onTimeTick() method.
             invalidate();
         }
 
         @Override
         public void onAmbientModeChanged(boolean inAmbientMode) {
             super.onAmbientModeChanged(inAmbientMode);
-            if (mLowBitAmbient) {
-                boolean antiAlias = !inAmbientMode;
-                mHourPaint.setAntiAlias(antiAlias);
-                mMinutePaint.setAntiAlias(antiAlias);
-                // mSecondPaint is not presented in ambient mode anyway.
-                mMinTickPaint.setAntiAlias(antiAlias);
-                mCharacterPaint.setAntiAlias(antiAlias);
-            }
-            // TODO(huangsz) restore to black and white.
+            mWatchFaceRenderer.setAmbientMode(inAmbientMode);
             invalidate();
             updateTimer();
         }
 
         @Override
         public void onDraw(Canvas canvas, Rect bounds) {
-            // Update the time
-            mCalendar.setTimeInMillis(System.currentTimeMillis());
-
-            int width = bounds.width();
-            int height = bounds.height();
-
-            canvas.drawBitmap(mBackgroundScaledBitmap, 0, 0, null);
-
-            // Find the center. Ignore the window insets so that, on round watches
-            // with a "chin", the watch face is centered on the entire screen, not
-            // just the usable portion.
-            float centerX = width / 2f;
-            float centerY = height / 2f;
-
-            // Draw the ticks.
-            drawTicks(canvas, centerX, centerY, 10, 12, mMinTickPaint);
-            drawTicks(canvas, centerX, centerY, 3, 60, mSecTickPaint);
-
-            // Compute rotations and lengths for the clock hands.
-            float seconds = mCalendar.get(Calendar.SECOND) +
-                    mCalendar.get(Calendar.MILLISECOND) / 1000f;
-            float secRot = seconds / 60f * TWO_PI;
-            float minutes = mCalendar.get(Calendar.MINUTE) + seconds / 60f;
-            float minRot = minutes / 60f * TWO_PI;
-            float hours = mCalendar.get(Calendar.HOUR) + minutes / 60f;
-            float hrRot = hours / 12f * TWO_PI;
-
-            float secLength = centerX - 20;
-            float minLength = centerX - 40;
-            float hrLength = centerX - 60;
-
-            // Only draw the second hand in interactive mode.
-            if (!isInAmbientMode()) {
-                float secX = (float) Math.sin(secRot) * secLength;
-                float secY = (float) -Math.cos(secRot) * secLength;
-                canvas.drawLine(centerX, centerY, centerX + secX, centerY +
-                        secY, mSecondPaint);
-            }
-
-            // Draw the minute and hour hands.
-            float minX = (float) Math.sin(minRot) * minLength;
-            float minY = (float) -Math.cos(minRot) * minLength;
-            canvas.drawLine(centerX, centerY, centerX + minX, centerY + minY,
-                    mMinutePaint);
-            float hrX = (float) Math.sin(hrRot) * hrLength;
-            float hrY = (float) -Math.cos(hrRot) * hrLength;
-            canvas.drawLine(centerX, centerY, centerX + hrX, centerY + hrY,
-                    mHourPaint);
-
-            // Draw the character.
-            float charX = centerX;
-            float charY = centerY * 1.6f;
-            canvas.drawText(TIP_TEXT, charX, charY, mCharacterPaint);
+            mWatchFaceRenderer.draw(canvas, bounds);
         }
 
         @Override
@@ -325,12 +211,7 @@ public class CharacterWatchFaceService extends CanvasWatchFaceService {
         public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {
             // Override the onSurfaceChanged() method to scale your background to fit the device
             // any time the view changes.
-            if (mBackgroundScaledBitmap == null
-                    || mBackgroundScaledBitmap.getWidth() != width
-                    || mBackgroundScaledBitmap.getHeight() != height) {
-                mBackgroundScaledBitmap = Bitmap.createScaledBitmap(mBackgroundBitmap,
-                        width, height, true /* filter */);
-            }
+            mWatchFaceRenderer.onSurfaceChanged(width, height);
             super.onSurfaceChanged(holder, format, width, height);
         }
 
@@ -349,48 +230,13 @@ public class CharacterWatchFaceService extends CanvasWatchFaceService {
         }
 
         @Override
-        public void onConnectionSuspended(int i) {
+        public void onConnectionSuspended(int cause) {
 
         }
 
         @Override
         public void onConnectionFailed(ConnectionResult connectionResult) {
 
-        }
-
-        private void drawTicks(Canvas canvas, float centerX, float centerY,
-                               int length, int divisions, Paint paint) {
-            float outerTickRadius = centerX;
-            float innerTickRadius = centerX - length;
-            for (int tickIndex = 0; tickIndex < divisions; tickIndex++) {
-                float tickRot = tickIndex * TWO_PI / divisions;
-                float innerX = (float) Math.sin(tickRot) * innerTickRadius;
-                float innerY = (float) -Math.cos(tickRot) * innerTickRadius;
-                float outerX = (float) Math.sin(tickRot) * outerTickRadius;
-                float outerY = (float) -Math.cos(tickRot) * outerTickRadius;
-                canvas.drawLine(centerX + innerX, centerY + innerY,
-                        centerX + outerX, centerY + outerY, paint);
-            }
-        }
-
-        private Paint createLinePaint(int color, float strokeWidth, Paint.Cap strokeCap) {
-            Paint paint = new Paint();
-            paint.setColor(color);
-            paint.setStrokeWidth(strokeWidth);
-            paint.setStrokeCap(strokeCap);
-            paint.setAntiAlias(true);
-            return paint;
-        }
-
-        private Paint createTextPaint(Resources resources, boolean isBold) {
-            Paint paint = new Paint();
-            paint.setColor(resources.getColor(R.color.black));
-            paint.setAntiAlias(true);
-            paint.setTextAlign(Paint.Align.CENTER);
-            paint.setTypeface(isBold ? BOLD_TYPEFACE : NORMAL_TYPEFACE);
-            paint.setTextSize(resources.getDimension(R.dimen.tip_text_size));
-            paint.setColor(resources.getColor(R.color.tip_text_color));
-            return paint;
         }
 
         private void maybeRegisterTimeZoneReceiver() {
